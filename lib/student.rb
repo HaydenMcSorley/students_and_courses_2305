8724b178a1e0687834136f4c399ac363a4b4681a
Hayden McSorley <haymcsorley@gmail.com>
2023-05-24 11:41:05 -0500

    Initial commit

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
 class Student
- 
-end
+   attr_reader :name, :age, :scores
+
+   def initialize
+    @name = name
+    @age = age
+    @scores = []
+   end
+
+   def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    return 0 if @scores.empty?
+
+    total = @scores.sum
+    average = total.to_f / @scores.length
+    average.round(1)
+  end 
+end   
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,5 +2,23 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  
+   describe '#name' do
+    it 'returns the name of the student' do
+        student = Student.new({name: "Morgan", age: 21})
+        expect(student.name).to eq("Morgan")
+    end
 end
+
+    describe '#age' do
+      it 'returns the age of the student' do
+          student = Student.new({name: "Morgan", age: 21})
+          expect(student.age).to eq(21)
+      end    
+    end
+
+    describe '#scores' do
+      it 'returns an empty array' do
+        student = Student.new({name: "Morgan", age: 21})
+        expect(student.scores).to eq([])
+      end    
+    end
